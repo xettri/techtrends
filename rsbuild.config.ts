538cde91032aab50eb8540be9d88bcca9dc7ee73
xettri <imbharatrawat@gmail.com>
2025-12-31 01:35:46 +0530

    use template html

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,6 +4,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    template: './public/index.html',
+  },
   output: {
     assetPrefix: '/techtrends/',
   }
